0ce754aa20f0243c8350840e628117c8c35b63ee
Milind Madhukar <m.milind15@gmail.com>
2020-12-05 11:53:47 +0530

    Credentials.json and Heroku Database Support Added

--- Giving2020/blog/admin.py
+++ Giving2020/blog/admin.py
@@ -3,8 +3,4 @@ from django.contrib import admin
 from blog.models import BlogPost, Comment, DownVote, Upvote, Announcement
 
 
-admin.site.register(BlogPost)
-admin.site.register(Comment)
-admin.site.register(DownVote)
-admin.site.register(Upvote)
-admin.site.register(Announcement)
+admin.site.register((BlogPost,Comment,DownVote,Upvote,Announcement))
